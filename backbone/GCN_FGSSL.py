5235ed9dd7a1be6bac1aace3c34fa63ad324e8c7
FooytheSheep <110292204+poetttttt@users.noreply.github.com>
2024-09-19 15:46:53 +0800

    Add files via upload

---
+++ backbone/GCN_FGSSL.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+
+class GCN_FGSSL(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=2, dropout=0.5):
+        super(GCN_FGSSL, self).__init__()
+        self.layers = nn.ModuleList()
+        self.dropout = dropout
+
+        if num_layers > 1:
+            self.layers.append(GCNConv(input_dim, hidden_dim))
+            for _ in range(num_layers - 2):
+                self.layers.append(GCNConv(hidden_dim, hidden_dim))
+            self.layers.append(GCNConv(hidden_dim, hidden_dim))
+        else:
+            self.layers.append(GCNConv(input_dim, hidden_dim))
+
+        self.classifier = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        for i, layer in enumerate(self.layers):
+            x = layer(x, edge_index)
+            if i != len(self.layers) - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=self.dropout, training=self.training)
+
+        features = x.clone()
+
+        x = self.classifier(features)
+        out = F.log_softmax(x, dim=1)
+
+        return out, features
+
+
